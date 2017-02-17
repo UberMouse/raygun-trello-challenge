087a08e2499d6b162c5ee43c7f06e2f2fa33d4a6
Taylor Lodge <ubermouse894@gmail.com>
2017-02-17 22:41:37 +1300

    Card show page displaying comments

--- RaygunTrelloChallenge/Controllers/CardsController.cs
+++ RaygunTrelloChallenge/Controllers/CardsController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Manatee.Trello;
 
 namespace RaygunTrelloChallenge.Controllers
 {
     public class CardsController : Controller
     {
         // GET: Boards/:id/Cards/:id
-        public ActionResult Show()
+        public ActionResult Show(string id)
         {
-            return View();
+            var card = new Card(id);
+
+            return View(card);
         }
     }
 }
